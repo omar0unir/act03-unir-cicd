3ad3e0e619c7f20af34bfac25e533abe08cf5c9f
omar0unir <167159223+omar0unir@users.noreply.github.com>
2024-06-11 00:39:23 -0500

    Update api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -2,6 +2,7 @@ import http.client
 import os
 import unittest
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 
 import pytest
 
@@ -21,3 +22,69 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
+        
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/5/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/5/5"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/12/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    
+    def test_api_divide_error_zero(self):
+        url = f"{BASE_URL}/calc/divide/2/0"
+        with self.assertRaises(HTTPError) as cm:
+            response = urlopen(url)
+        self.assertEqual(cm.exception.status,  http.client.BAD_REQUEST)
+    
+    def test_api_power(self):
+        url = f"{BASE_URL}/calc/power/2/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    
+    def test_api_sqrt(self):
+        url = f"{BASE_URL}/calc/sqrt/9"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    
+    def test_api_sqrt_error_negative_number(self):
+        url = f"{BASE_URL}/calc/sqrt/-10"
+        with self.assertRaises(HTTPError) as cm:
+            response = urlopen(url)
+        self.assertEqual(cm.exception.status,  http.client.BAD_REQUEST)
+    
+    def test_api_logbase10(self):
+        url = f"{BASE_URL}/calc/logbase10/1000"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    
+    def test_api_logbase10_error_zero(self):
+        url = f"{BASE_URL}/calc/logbase10/0"
+        with self.assertRaises(HTTPError) as cm:
+            response = urlopen(url)
+        self.assertEqual(cm.exception.status,  http.client.BAD_REQUEST)
+    
+    def test_api_logbase10_error_string(self):
+        url = f"{BASE_URL}/calc/logbase10/cero"
+        with self.assertRaises(HTTPError) as cm:
+            response = urlopen(url)
+        self.assertEqual(cm.exception.status,  http.client.BAD_REQUEST)
